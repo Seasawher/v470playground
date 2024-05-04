eb10ab925263fc0972d1c498fabe17bc14ba33b6
Seasawher <tellagoodstory@icloud.com>
2024-05-04 16:53:57 +0900

    remove says

--- V470playground/Basic.lean
+++ V470playground/Basic.lean
@@ -1,10 +1,10 @@
 import Mathlib.Tactic
 
-set_option says.verify true
-
 variable (n m : Nat)
 
 example (h : n + m = 0) : n = 0 ↔ m = 0 := by
-  -- success in v4.7.0
-  rw? says
-    rw [propext (eq_zero_iff_eq_zero_of_add_eq_zero h)]
+  /-
+  Try this: rw [propext (eq_zero_iff_eq_zero_of_add_eq_zero h)]
+  -- "no goals"
+  -/
+  rw?
