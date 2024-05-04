a33a8fe50db9bf6f7ea7a00bef68e549696a6b6c
Seasawher <tellagoodstory@icloud.com>
2024-05-04 16:31:26 +0900

    initial commit

---
+++ V470playground.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `V470playground` library.
+-- Import modules here that should be built as part of the library.
+import «V470playground».Basic
---
+++ V470playground/Basic.lean
@@ -0,0 +1,8 @@
+import Mathlib.Tactic
+
+variable (n m : Int)
+
+example (h : n + m = 0) : n = 0 ↔ m = 0 := by
+  -- success in v4.7.0
+  rw? says
+    rw [propext (eq_zero_iff_eq_zero_of_add_eq_zero h)]
